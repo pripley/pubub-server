25f70a4383d22ac63a576135c5415ede838ad6da
Paul Ripley <paulripley@hotmail.com>
2021-11-13 09:40:43 -0500

    deleted review

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,5 @@
 module.exports = {
   User: require("./usercontroller"),
   Brewery: require("./brewerycontroller"),
-  Beer: require("./beercontroller"),
-  Review: require("./reviewcontroller"),
+  Beer: require("./beercontroller")  
 };
